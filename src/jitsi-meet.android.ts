b50c8e9ad377b34a90adb340ef385f0dfffb753b
dborba <daniel@witfylabs.com>
2020-05-04 18:20:25 -0300

    turning back to android version 1.1.0 of the code that works

--- src/jitsi-meet.android.ts
+++ src/jitsi-meet.android.ts
@@ -3,15 +3,19 @@ import { NativescriptJitsiMeetConferenceOptions } from "./jitsi-meet.common";
 
 export class NativescriptJitsiMeet  {
 	private _eventListeners: Array<{ event: string, callback: Function }>;
+	private _serverURL: string = '';
+	private _jitsiView: io.witfy.jitsiconnector.JitsiConnectorActivity;
 
-	constructor() {
+	constructor(serverURL?: string) {
+		this._eventListeners = new Array();
+		this._serverURL = !!serverURL ? serverURL : 'https://meet.jit.si';
 	}
 
 	public startMeeting(options: NativescriptJitsiMeetConferenceOptions) {
 		const context = application.android.context;
-		const jitsiConnector = new io.witfy.jitsiconnector.JitsiConnectorActivity();
+		this._jitsiView = new io.witfy.jitsiconnector.JitsiConnectorActivity();
 
-		jitsiConnector.setJitsiStateListener(new io.witfy.jitsiconnector.JitsiStateListener({
+		this._jitsiView.setJitsiStateListener(new io.witfy.jitsiconnector.JitsiStateListener({
 			onConferenceStarted: (test: string) => {
 				console.log('## ' + test);
 			},
@@ -30,7 +34,7 @@ export class NativescriptJitsiMeet  {
 		const configuration = this._buildObjectOptions(options);
 
 		if (configuration) {
-			jitsiConnector.startVideo(context, options.roomName, configuration);
+			this._jitsiView.startVideo(context, options.roomName, configuration);
 		}
 	}
 
@@ -46,7 +50,7 @@ export class NativescriptJitsiMeet  {
 				featureFlagsMap.setCallIntegrationEnabled(
 					!!options.featureFlags.callIntegration 
 						? options.featureFlags.callIntegration : false);
-				
+
 				featureFlagsMap.setChatEnabled(
 					!!options.featureFlags.chatEnabled
 						? options.featureFlags.chatEnabled : false);
@@ -84,25 +88,25 @@ export class NativescriptJitsiMeet  {
 
 		return configuration;
 	}
-	
-	private _callEventListeners(eventName: string, data: NSDictionary<string, any>) {
-        const eventListener = this._eventListeners.find(eventListener => eventListener.event === eventName);
-        if (!!eventListener) {
-            eventListener.callback(data.allValues.firstObject);
-        }
-	}
-	
-	on(eventName: string, callback: Function) {
-        if (eventName === 'conferenceWillJoin'
-                || eventName === 'conferenceJoined'
-                    || eventName === 'conferenceTerminated') {
-            if (!this._eventListeners.find(eventListener => eventListener.event === eventName)) {
-                this._eventListeners.push({ event: eventName, callback: callback });
-            }
-        }
-    }
 
-    addEventListener(event: string, callback: Function) {
-        this.on(event, callback);
-    }
+	private _callEventListeners(eventName: string, data: NSDictionary<string, any>) {
+		const eventListener = this._eventListeners.find(eventListener => eventListener.event === eventName);
+		if (!!eventListener) {
+			eventListener.callback(data.allValues.firstObject);
+		}
+	}
+
+	on(eventName: string, callback: Function) {
+		if (eventName === 'conferenceWillJoin'
+			|| eventName === 'conferenceJoined'
+				|| eventName === 'conferenceTerminated') {
+			if (!this._eventListeners.find(eventListener => eventListener.event === eventName)) {
+				this._eventListeners.push({ event: eventName, callback: callback });
+			}
+		}
+	}
+
+	addEventListener(event: string, callback: Function) {
+		this.on(event, callback);
+	}
 }
